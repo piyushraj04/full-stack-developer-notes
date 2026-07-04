fa6c7cfd965c0e8e3b3a4b374a7cd97bb37d1191
Piyush Raj <164622139+piyushraj04@users.noreply.github.com>
2026-07-04 15:58:01 +0530

    JS Array Methods

---
+++ 03-JavaScript/Assignments/Array/01_Arrays.js
@@ -0,0 +1,63 @@
+let arr = [1,true,"Piyush",NaN,true,null,undefined,NaN,4,1]
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[3]);
+// for(let i = 1;i < arr.length;i++){
+//     console.log(arr[i]);
+// }
+// arr[3] = false;
+// console.log(arr[3]);
+
+// Array Methods:---
+
+
+//push--add new elements at the end of the array
+
+// arr.push(5,6,7)
+// console.log("after push--> ");
+// console.log(arr);
+
+//pop--removes from last
+// arr.pop()
+// console.log("after pop--> ");
+// console.log(arr);
+
+
+//unshift--add element in starting of the array
+// arr.unshift("Raj")
+// console.log("after unshift--> ");
+// console.log(arr);
+
+//shift--removes element from starting of the array
+// arr.shift()
+// console.log("after shift--> ");
+// console.log(arr);
+
+//indexOf---gives index of the element in array
+// console.log("index of -->")
+// console.log(arr.indexOf("Piyush"));
+
+//lastIndexOf--return last occurence of element
+// console.log("last index of --->");
+console.log(arr.lastIndexOf("Piyush",8));
+
+// console.log(arr.lastIndexOf(NaN));
+// console.log(arr.indexOf(null));
+// console.log(arr[3]);
+
+
+
+
+//includes--arr.includes(element to be search, from which index want to start)
+// console.log(arr);
+
+// console.log(arr.includes(1,8));
+
+
+
+
+
+
+
+
+
