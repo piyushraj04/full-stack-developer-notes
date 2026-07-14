d6976fdd31816a7aa02033515507d3f7683e8460
piyushraj04 <piyush004.raj@gmail.com>
2026-07-14 12:13:57 +0530

    Collections methods and Some core OOP designs

--- 05-Core-Java/01-Basics/OthersImpStuffs/VarLearning.java
+++ 05-Core-Java/01-Basics/OthersImpStuffs/VarLearning.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.TreeSet;
 
 class VarLearning{
     // int x = 10;
@@ -35,6 +37,30 @@ class VarLearning{
 
             System.out.println(el);
         }
+
+        al.add(5);
+        al.add(8);
+        al.add(9);
+        al.add(5);
+        al.add(5);
+        al.add(5);
+        System.out.println(al);
+        System.out.println("Frequency -- "+ Collections.frequency(al, 5));
+        
+
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        ts.add(8);
+        ts.add(5);
+        ts.add(6);
+        ts.add(9);
+        System.out.println(ts);
+        System.out.println(ts.floor(5)); //5
+        System.out.println(ts.floor(9)); //9
+        System.out.println(ts.floor(7)); //6
+        System.out.println(ts.floor(11)); //9
+
+
 
 
 
