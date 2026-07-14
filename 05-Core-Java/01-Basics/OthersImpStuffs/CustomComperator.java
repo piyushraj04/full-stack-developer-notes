f480710e6e7e95e2550c18a93e087ec2d5559140
piyushraj04 <piyush004.raj@gmail.com>
2026-07-14 12:21:20 +0530

    Implemented a custom comparator

---
+++ 05-Core-Java/01-Basics/OthersImpStuffs/CustomComperator.java
@@ -0,0 +1,40 @@
+// package 01-Basics.OthersImpStuffs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.swing.undo.CompoundEdit;
+
+public class CustomComperator {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(7);
+        list.add(3);
+        list.add(12);
+
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+
+        //custom
+
+        Collections.sort(list, new Comparator<Integer>(){
+            @Override
+            public int compare(Integer o1,Integer o2){
+                if(o1<o2){
+                    return 1;
+                }
+                else if(o1>o2){
+                    return -1;
+                }
+                return 0;
+            }
+        });
+
+        System.out.println(list);
+    }
+    
+}
