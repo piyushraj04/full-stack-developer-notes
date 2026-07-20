941f3c0d5b794d1583c539d7c15c03d357f1e862
piyushraj04 <piyush004.raj@gmail.com>
2026-07-20 15:42:59 +0530

    char hashing

--- 11-DSA/Hashing/NoOfOcc.java
+++ 11-DSA/Hashing/NoOfOcc.java
@@ -2,7 +2,7 @@ class NoOfOcc{
     public static int max(int[] arr){
         int max = arr[0];
         int length = arr.length;
-        for(int i = 1; i < length-1; i++){
+        for(int i = 1; i < length; i++){
             if(max<arr[i]){
                 max = arr[i];
             }
@@ -18,15 +18,31 @@ class NoOfOcc{
             hash[arr[i]]++;
         }
     }
+        public static void freChar(char[] arr1,int[] hash2){
+        
+        int orgLen = arr1.length;
+        
+        for(int i = 0; i < orgLen; i++){
+            ((hash2[((int)(arr1[i]))-97]))++;
+            // System.out.println((int)(arr1[i]));
+        }
+        
+        
+    }
+
 
     public static void main(String[] args) {
         int[] arr = {1,5,6,5,6,7,4};
+        char[] arr1 = {'a','b','d','e','r'};
+        int[] hash2 = new int[26];
+       
         int size = NoOfOcc.max(arr)+1;
         int[] hash = new int[size];
-        // System.out.println(NoOfOcc.max(arr));
-        NoOfOcc.freArr(arr,hash);
-         
-        System.out.println("The frequency of "+ 5 + " is "+hash[5]);
+        NoOfOcc.freChar(arr1, hash2);
+        for(int i = 0; i < 26; i++){
+            System.out.println((char)(97+i)+"--"+hash2[i]);
+        }
+
 
     }
 }
