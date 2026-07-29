787aae1f2a1873273c748c7ef9031cf97230c957
piyushraj04 <piyush004.raj@gmail.com>
2026-07-29 23:08:06 +0530

    React and DSA

--- 11-DSA/Strings/ReverseListChar.java
+++ 11-DSA/Strings/ReverseListChar.java
@@ -1,18 +1,32 @@
 
 
 class ReverseListChar{
-
-    public static void main(String[] args) {
-       String str = "003546";
-       int num = Integer.parseInt(str);
-       int[] number = {num};
-       for(int i = 0; i < number.length;i++){
-        for(int j = i ; j < number.length; j++){
-            int n = 
+public static boolean rotateString(String s, String goal) {
+        //your code goes here
+        if(s.length()!=goal.length()) return false;
+        if(s.equals(goal)) return true;
+        StringBuilder sb = new StringBuilder(s);
+        int i = s.length()-1;
+        while(0<=i){
+            String res = shiftOneChar(sb).toString();
+            if(res.equals(goal)){
+                return true;
+            }
+            i--;
         }
-       }
-
-        
-        
+        return false;
     }
+    public static StringBuilder shiftOneChar(StringBuilder sb){
+        Character temp = sb.charAt(0);
+        for(int i = 0; i < sb.length()-1; i++){
+            Character nextChar = sb.charAt(i+1);
+            sb.setCharAt(i,nextChar);
+        }
+        sb.insert(sb.length()-1,temp);
+        return sb;
+    }
+   public static void main(String[] args){
+      System.out.println(ReverseListChar.rotateString("abcde","cdeab"));
+      
+                         }
 }
