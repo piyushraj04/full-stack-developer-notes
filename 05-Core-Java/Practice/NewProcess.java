cb76145c4ad02923c7c151b7dcb5f8174de3b1d7
piyushraj04 <piyush004.raj@gmail.com>
2026-08-02 08:53:48 +0530

    java

--- 05-Core-Java/Practice/NewProcess.java
+++ 05-Core-Java/Practice/NewProcess.java
@@ -1,21 +1,6 @@
 class NewProcess{
-    static int num;
-    
-    NewProcess(){
-        System.out.println("I'm invoked...");
-        System.out.println("2");
-    }
-    {
-        System.out.println("3");
-        System.out.println("I'm non-static/instance block...");
-    }
     static{
-        System.out.println("1");
-        System.out.println("I'm static block...");
-        System.out.println("Num is :"+num);
+        System.out.println("Piyush Rajj");
     }
-    public static void main(String[] args) {
-        NewProcess newProcess = new NewProcess();
-        
-    }
+  
 }
