f4a414c122d1024931499fd74f43fc7fde83e5f2
piyushraj04 <piyush004.raj@gmail.com>
2026-08-18 01:44:33 +0530

    recursion dsa

--- 11-DSA/Recursion/Test.java
+++ 11-DSA/Recursion/Test.java
@@ -1,15 +1,51 @@
+import java.util.Scanner;
+
 class Test{
     public void printName(int cnt){
         //base condn
         if(cnt==0){
             return;
         }
-        System.out.println("Piyush");
+        
         printName(--cnt);
+        System.out.println("Piyush");
         //recursive call
+    }
+
+    public void nTimesX(int x, int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(x);
+        nTimesX(x, --n);
+    }
+
+    public void oneToN(int x,int n){
+        if(x==n){
+            System.out.println(x);
+            return;
+        }
+        System.out.println(x);
+        oneToN(++x,n);
+         
+
+         
+           //0--> last time sooooooooooooo:-----
+       
     }
     public static void main(String[] args) {
         Test t = new Test();
-        t.printName(4);        
+        // t.printName(4); 
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter x : ");
+        // int x = sc.nextInt();
+        
+        // System.out.println("Enter n : ");
+        // int n = sc.nextInt();
+
+        // t.nTimesX(x,n);
+
+        t.oneToN(1,5);
+        sc.close();
     }
 }
